f9cae53a402e888011a681ad6c2b817db5a3ada0
Mathias Schulz <mathias@schulz.net.br>
2023-12-14 14:27:57 -0300

    feat(image): add apod image to tweet

--- create_tweet.py
+++ create_tweet.py
@@ -2,8 +2,8 @@ import logging
 import os
 import coloredlogs
 import requests
+import tweepy
 from dotenv import load_dotenv
-from tweepy import Client
 
 load_dotenv()
 
@@ -13,13 +13,13 @@ coloredlogs.install()
 NASA_API_URL = "https://api.nasa.gov"
 
 
-def __twitter_client() -> Client:
-    """Inicia um client para comunicação com Twitter
+def __twitter_client() -> tweepy.Client:
+    """Inicia um client para comunicação com Twitter API v2.0
 
     Returns:
-        Client: Retorna o client
+        tweepy.Client: Retorna o client
     """
-    return Client(
+    return tweepy.Client(
         consumer_key=os.getenv("TWITTER_API_KEY"),
         consumer_secret=os.getenv("TWITTER_API_KEY_SECRET"),
         access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
@@ -27,23 +27,64 @@ def __twitter_client() -> Client:
     )
 
 
-def __create_tweet(message: str, in_reply_to_tweet_id: str = None) -> str:
+def __twitter_client_v1() -> tweepy.API:
+    """Inicia um client para comunicação com Twitter API v1.1
+
+    Returns:
+        tweepy.API: Retorna o client
+    """
+    auth = tweepy.OAuth1UserHandler(
+        os.getenv("TWITTER_API_KEY"), os.getenv("TWITTER_API_KEY_SECRET")
+    )
+    auth.set_access_token(
+        os.getenv("TWITTER_ACCESS_TOKEN"),
+        os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+    )
+    return tweepy.API(auth)
+
+
+def __create_tweet(
+    message: str, in_reply_to_tweet_id: str = None, file_url: str = None
+) -> str:
     """Realiza a criação de um novo tweet
     Full doc: https://docs.tweepy.org/en/latest/client.html#tweepy.Client.create_tweet
 
     Args:
         message (str): Mensagem do tweet (No máximo 280 caracteres)
         in_reply_to_tweet_id (str, optional): Tweet pai/Tweet que será respondido. Defaults to None.
+        file_url (str, optional): Url da imagem para adicionar no tweet
 
     Returns:
         str: ID do tweet criado
     """
     client = __twitter_client()
 
+    media_ids = None
+    if file_url:
+        filename = "tmp/sample.jpg"
+        request = requests.get(file_url, stream=True, timeout=5)
+
+        with open(filename, "wb") as image:
+            for chunk in request:
+                image.write(chunk)
+
+        # os.remove(filename)
+
+        # Full doc: https://docs.tweepy.org/en/latest/api.html#tweepy.API.media_upload
+        client_v1 = __twitter_client_v1()
+        response_file = client_v1.media_upload(filename=filename)
+        media_ids = [response_file.media_id]
+
     response = client.create_tweet(
-        text=message, in_reply_to_tweet_id=in_reply_to_tweet_id
+        text=message,
+        in_reply_to_tweet_id=in_reply_to_tweet_id,
+        media_ids=media_ids,
     )
-    logging.warning(f"Tweet: https://x.com/SpaceRoverBot/status/{response.data['id']}")
+    logging.warning(f"TWEET > https://x.com/SpaceRoverBot/status/{response.data['id']}")
+
+    # TODO:
+    # TODO: client.like()
+    # TODO:
     return response.data["id"]
 
 
@@ -60,10 +101,7 @@ def __nasa_apod() -> dict:
     )
 
     data = response.json()
-
-    logging.info("NASA APOD")
-    logging.info(f"status_code: {response.status_code}")
-    logging.info(f"response: {data}")
+    logging.info(f"APOD [{response.status_code}] > {data}")
 
     return data
 
@@ -86,13 +124,13 @@ def __main():
 
         message = "\n".join(build_message)
 
-        tweet_id = __create_tweet(message)
+        tweet_id = __create_tweet(message=message, file_url=apod_info["hdurl"])
 
         explanation = f"Description: {apod_info['explanation']}"
         if len(explanation) > 280:
             explanation = f"{explanation[:277]}..."
 
-        __create_tweet(explanation, tweet_id)
+        __create_tweet(message=explanation, in_reply_to_tweet_id=tweet_id)
 
         logging.info("Tweet posted with success!")
     except Exception as error:
